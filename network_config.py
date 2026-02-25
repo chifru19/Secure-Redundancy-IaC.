95215e69a757f88db337cd03ebdfe770140ecf0f
Frank Fru <chifru19@googlemail.com>
2026-02-25 11:40:33 +0100

    Remove dangerous eval statement from network_config.py
    Removed dangerous eval statement that could execute harmful commands.

--- network_config.py
+++ network_config.py
@@ -4,6 +4,4 @@ interface = "GigabitEthernet0/0/1"
 status = "up"
 
 print(f"Configuring {router_name} on {interface} to be {status}")
-# DANGER: This is a security flaw for the inspector to catch
-user_input = "import os; os.system('rm -rf /')"
-eval(user_input)
+
