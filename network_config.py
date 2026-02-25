1c3bf3046fcbd32e8fa3f52ec5d46c09361cae93
Frank Fru <chifru19@googlemail.com>
2026-02-25 11:37:47 +0100

    Introduce eval with user input in network_config.py
    Added dangerous eval statement that executes user input.

--- network_config.py
+++ network_config.py
@@ -4,3 +4,6 @@ interface = "GigabitEthernet0/0/1"
 status = "up"
 
 print(f"Configuring {router_name} on {interface} to be {status}")
+# DANGER: This is a security flaw for the inspector to catch
+user_input = "import os; os.system('rm -rf /')"
+eval(user_input)
